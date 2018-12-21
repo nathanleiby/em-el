714d87d37302f258e58d00285edd8d15b7129435
Nathan Leiby <nathan.leiby@clever.com>
2018-12-20 18:49:55 -0800

    app.py - disable debug mode

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ print(("* Loading Keras model and Flask starting server..."
 load_model()
 
 port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
+app.run(host='0.0.0.0', port=port)
