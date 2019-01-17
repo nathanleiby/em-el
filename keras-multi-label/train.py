ab3e957328d9bcb21ee20f50c368c60911727b4c
Nick Leiby <nick.leiby@twosixlabs.com>
2019-01-16 22:48:05 -0500

    Refactors train.py

--- keras-multi-label/train.py
+++ keras-multi-label/train.py
@@ -1,5 +1,5 @@
 # USAGE
-# python train.py --dataset dataset --model cards_model_output --labelbin mlb.pickle
+# python train.py --dataset dataset
 
 # set the matplotlib backend so figures can be saved in the background
 import matplotlib
@@ -7,6 +7,7 @@ import matplotlib
 matplotlib.use("Agg")
 
 import argparse
+from datetime import datetime
 import json
 import os
 import pickle
@@ -32,9 +33,9 @@ INIT_LR = 1e-3
 BATCH_SIZE = 32
 IMAGE_DIMS = (96, 96, 3)
 RANDOM_SEED = 42
+TIME_STRING_FORMAT = "%Y%m%d-%H%M"
 
-
-def load_image_and_label_files():
+def load_image_and_label_files(random_seed):
     # grab the image paths and randomly shuffle them
     print("[INFO] loading images...")
     # read the labels file
@@ -80,7 +81,7 @@ def load_image_and_label_files():
         label_map[name] = label_list
 
     image_paths = sorted(list(paths.list_images(imgdir)))
-    random.seed(RANDOM_SEED)
+    random.seed(random_seed)
     random.shuffle(image_paths)
 
     return image_paths, label_map
@@ -123,22 +124,15 @@ def preprocess_data(image_paths, label_map):
     # loop over each of the possible class labels and show them
     for (i, label) in enumerate(mlb.classes_):
         print("{}. {}".format(i + 1, label))
-
-    # save the multi-label binarizer to disk
-    # print("[INFO] serializing label binarizer...")
-    # f = open(args["labelbin"], "wb")
-    # f.write(pickle.dumps(mlb))
-    # f.close()
-    labels = np.array(labels)
     return labels, data, mlb
 
 
 def run_model(train_x, train_y, test_x, test_y, num_output_classes, model_output_name='cards_model_output'):
 
     # val_loss is cross-validation loss, loss is testing loss
-    checkpoint_callback = ModelCheckpoint(model_output_name, monitor='val_loss', save_best_only=True)
+    checkpoint_callback = ModelCheckpoint(model_output_name + ".model", monitor='val_loss', save_best_only=True)
     # stops the fit after {patience} epochs where the the variable {monitor} doesn't change my more than {min_delta}
-    stopping_callback = EarlyStopping(monitor='val_loss', min_delta=0, patience=3)
+    stopping_callback = EarlyStopping(monitor='val_loss', min_delta=0, patience=10)
     callbacks_list = [checkpoint_callback, stopping_callback]
 
     # construct the image generator for data augmentation
@@ -181,30 +175,24 @@ def run_model(train_x, train_y, test_x, test_y, num_output_classes, model_output
         steps_per_epoch=len(train_x) // BATCH_SIZE,
         epochs=EPOCHS,
         verbose=1,
-        # callbacks_list=callbacks_list
+        callbacks=callbacks_list
     )
-
-    # save the model to disk
-    # print("[INFO] serializing network...")
-    # model.save(MODEL_OUTPUT_NAME)
-
     return fitted_model
-# 
-# def plot_model_summary(plot_filename):
-# 
-# 
-#     # plot the training loss and accuracy
-#     plt.style.use("ggplot")
-#     plt.figure()
-#     plt.plot(np.arange(0, EPOCHS), fitted_model.history["loss"], label="train_loss")
-#     plt.plot(np.arange(0, EPOCHS), fitted_model.history["val_loss"], label="val_loss")
-#     plt.plot(np.arange(0, EPOCHS, fitted_model.history["acc"], label="train_acc")
-#     plt.plot(np.arange(0, EPOCHS), fitted_model.history["val_acc"], label="val_acc")
-#     plt.title("Training Loss and Accuracy")
-#     plt.xlabel("Epoch #")
-#     plt.ylabel("Loss/Accuracy")
-#     plt.legend(loc="upper left")
-#     plt.savefig(args["plot"])
+
+
+def plot_model_summary(plot_filename, fitted_model):
+    # plot the training loss and accuracy
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(np.arange(0, len(fitted_model.history["loss"])), fitted_model.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, len(fitted_model.history["val_loss"])), fitted_model.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, len(fitted_model.history["acc"])), fitted_model.history["acc"], label="train_acc")
+    plt.plot(np.arange(0, len(fitted_model.history["val_acc"])), fitted_model.history["val_acc"], label="val_acc")
+    plt.title("Training Loss and Accuracy")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend(loc="upper left")
+    plt.savefig(plot_filename)
 
 
 if __name__ == "__main__":
@@ -216,32 +204,39 @@ if __name__ == "__main__":
         required=True,
         help="path to input dataset (i.e., directory of images)",
     )
-    ap.add_argument("-m", "--model", required=True, help="path to output model")
-    ap.add_argument(
-        "-l", "--labelbin", required=True, help="path to output label binarizer"
-    )
-    ap.add_argument(
-        "-p",
-        "--plot",
-        type=str,
-        default="plot.png",
-        help="path to output accuracy/loss plot",
-    )
+    
     args = vars(ap.parse_args())
-    MODEL_OUTPUT_NAME = args["model"]
+    random_seed = 42
+    for i in range(3):
+        random_seed += i
+        run_name = "%s_model_run_seed_%s" % (datetime.utcnow().strftime(TIME_STRING_FORMAT), random_seed)
 
-    image_paths, label_map = load_image_and_label_files()
-    data, labels, label_binarizer = preprocess_data(image_paths, label_map)
+        image_paths, label_map = load_image_and_label_files(random_seed)
+        labels, data, label_binarizer = preprocess_data(image_paths, label_map)
 
-    # partition the data into training and testing splits using 80% of
-    # the data for training and the remaining 20% for testing
-    train_x, test_x, train_y, test_y = train_test_split(data,
-                                                        labels,
-                                                        test_size=0.2,
-                                                        random_state=RANDOM_SEED)
+        # partition the data into training and testing splits using 80% of
+        # the data for training and the remaining 20% for testing
+        train_x, test_x, train_y, test_y = train_test_split(data,
+                                                            labels,
+                                                            test_size=0.2,
+                                                            random_state=random_seed)
 
-    fitted_model = run_model(train_x, 
-                             train_y,
-                             test_x,
-                             test_y, 
-                             num_output_classes=len(label_binarizer.classes_))
+        fitted_model = run_model(train_x,
+                                 train_y,
+                                 test_x,
+                                 test_y,
+                                 num_output_classes=len(label_binarizer.classes_),
+                                 model_output_name=os.path.join("models", run_name)
+                                 )
+
+        plot_model_summary(os.path.join("models", run_name + ".plot.png"), fitted_model)
+        with open(os.path.join("models", run_name + '_data'), 'wb') as fout:
+            pickle.dump((train_x, test_x, train_y, test_y), fout)
+
+        # plot classification auc
+        # plot classification error by data type
+
+        # save the multi-label binarizer to disk
+        print("[INFO] serializing label binarizer...")
+        with open(os.path.join("models", run_name + '_labels'), "wb") as fout:
+            pickle.dump(label_binarizer, fout)
